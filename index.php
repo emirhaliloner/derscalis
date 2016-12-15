0cb99c5c68cad17c9df7a555fb1e005368db2ce3
emrh <12542552.emirhaliloner@gmail.com>
2016-12-15 15:57:27 +0300

    ilk açılış sayfası

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>WebteDers</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="htttp://www.binarytheme.com">WEBTEDERS </a>
+        </div>
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home">ANASAYFA</a>
+                </li>
+                <li><a href="#about">HAKKIMIZDA</a>
+                </li>
+                <li><a href="#services">HİZMETLER</a>
+                </li>
+                <li><a href="#contact">İLETİŞİM</a>
+                </li>
+                <li><a href="login.php">GİRİŞ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container" id="home">
+    <div class="row text-center ">
+        <div class="col-md-3 head-main ">
+            <i class="fa fa-bell color-blue "></i>
+            <h3>  <a href="#home">ANASAYFA</a></h3>
+
+        </div>
+        <div class="col-md-3 head-main">
+
+            <i class="fa fa-edit color-blue "></i>
+            <h3>  <a href="#about">HAKKIMIZDA</a></h3>
+
+        </div>
+        <div class="col-md-3 head-main">
+
+            <i class="fa fa-globe color-blue "></i>
+            <h3>  <a href="#services">HİZMETLER</a></h3>
+
+        </div>
+        <div class="col-md-3 head-main">
+            <i class="fa fa-headphones color-blue"></i>
+            <h3>  <a href="#contact">İLETİŞİM</a></h3>
+
+        </div>
+    </div>
+</div>
+<section class="for-full-back color-light " id="about">
+    <div class="container">
+
+        <div class="row text-center">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>HAKKIMIZDA</h1>
+                <h4>
+                    <strong>WebteDers Lise ve Üniversitelilere yönelik bir sitedir.
+                    </strong>
+                </h4>
+            </div>
+
+        </div>
+        <div class="row text-center">
+            <div class="col-md-12">
+                <p>
+                    Bu projenin amacı lise ve üniversiteli öğrenciler için herhangi bir ders ya da sınav için kaynak sıkıntısı çekmemelerini sağlamaktır.
+                    Bu eğitim platformu sayesinde öğrenciler istedikleri konuyu basit ve anlaşılır bir yapıyla öğrenebileceklerdir.
+                    Öğrencilerinde paylaşımlarıyla konular çoğalıp kullanıcıların sıkıntı çekmemeleri sağlanmaktadır.
+                    Bu proje sayesinde öğrenciler doğru bilgiler ışığında hareket edebileceklerdir.
+                    Proje videolu, pdf formatındaki anlatım ile öğrencilere fayda sağlamayı amaçlamaktadır.
+                    Proje öğrencilerin ders notlarını paylaşabilecekleri, çalışmalarla ilgili öneride bulunulabilecekleri bir platform olacaktır.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+<section class="for-full-back color-white" id="services">
+    <div class="container">
+
+        <div class="row text-center">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>HİZMETLER</h1>
+                <h4>
+                    <strong>WebteDersin amacı lise ve üniversiteli öğrencilere kaynak sıkıntısı çektirmemektir.
+                    </strong>
+                </h4>
+            </div>
+
+        </div>
+
+        <div class="row  space-pad">
+            <div class="col-md-3 ">
+
+
+
+            </div>
+            <div class="col-md-6 ">
+                <p class="text-center">
+                    Sitede öğrenciler çalıştıkları konularda test olup anında başarı durumunu öğrenebilecekler ve
+                    yanlışlarına göre de nerede eksiklikleri olduğuna bakabileceklerdir.
+                    Bunun yanında psikolojik ve dikkat testleriyle de öğrencinin ruhsal ve bedensel durumu hakkında anında bilgilendirme yapılacaktır.
+                    Bu test sonuçları isteğe göre öğrencinin ebeveynine mesaj veya mail olarak gönderilme işlemi sağlatılacaktır.
+                    Ebeveynler de bu sonuçlara göre daha doğru bir şekilde hareket edeceklerdir.
+                </p>
+                <p>
+
+                </p>
+                <p>
+                    Projeye eğitimle ilgili haberler, makaleler, öneriler ve hangi mesleklerin yükselişte olduğuyla ilgili bilgilere de yer verilecektir.
+                    Öğrenciler bu durumlara bakarak hayatlarına bir yön vereceklerdir.
+                    Kullanıcılardan alınacak yeni fikirlerle de sitenin gelişimi üzerinde durulacaktır.
+                    Kullanıcılar üye olmadan derslere ve testlere başlayamayacaklardır.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="for-full-back color-white " id="contact">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>İLETİŞİM</h1>
+                <h4>
+                    <strong>WEBTEDERS te mesajlarınıza en kısa zamanda cevap verilmeye çalışılacaktır.
+                    </strong>
+                </h4>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-5 contact-cls">
+                <h3>Konumumuz </h3>
+                <div>
+                    <span><i class="fa fa-home"></i>&nbsp; Ülke/Şehir: Türkiye/Elazığ</span>
+                    <br />
+                    <span><i class="fa fa-phone"></i>&nbsp;Telefone:(___)___ ____ </span>
+                    <br />
+                    <span><i class="fa fa-envelope-o"></i>&nbsp;E-Mail:12542552.emirhaliloner@gmail.com</span>
+                    <br />
+
+                    <span><i class="fa fa-globe"></i>&nbsp;Web: www.webteders.com</span>
+                    <br /> <br />
+
+                </div>
+
+            </div>
+            <div class="col-md-7">
+                <form>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="İsminiz">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="Email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="3" placeholder="Mesajınız..."></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default">GÖNDER</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</section>
+<div class="for-full-back " id="footer">
+    2016 www.webteders.com
+</div>
+<script src="assets/plugins/jquery-1.10.2.js"></script>
+<script src="assets/plugins/bootstrap.js"></script>
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
